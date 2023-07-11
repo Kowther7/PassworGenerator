089e4ffff96ac6376dc46f53eb15873ca62c01df
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-07-11 14:18:41 -0700

    declares variables

--- devlops/script.js
+++ devlops/script.js
@@ -10,3 +10,10 @@ var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+// Declaring Variables 
+var lengthOfPassword = "";
+var confirmLowerLetter;
+var confirmUpperLetter;
+var confirmSymbol;
+var confirmNumber;
+
