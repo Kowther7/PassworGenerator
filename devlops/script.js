8a01a4d3c552fdd1637726d28c4f02c358035304
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-07-11 10:59:34 -0700

    adds the arrays of upper and lower case, number and symbol

--- devlops/script.js
+++ devlops/script.js
@@ -3,3 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Arrays of lower letter, upper letter, numbers, and symbols 
+var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
